24bef84b3c61c1eabaace887ac7767e0001e53d2
Ivana <bosilkovska.ivana@gmail.com>
2020-11-18 12:26:19 +0100

    changed main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 class HelloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+
+        System.out.println("Hello");
         }
+
     }
 
