2dbe760861e91fd9e01a484105c914e8b5db7ad8
Ivana <bosilkovska.ivana@gmail.com>
2021-05-20 20:57:56 +0200

    new changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,7 @@ class HelloWorld{
     public static void main(String[] args) {
         System.out.println("Hello World!!");
 
-        System.out.println("4" +
-                "5;iuhi");
+        System.out.println("kjhkjfdsh");
         }
 
     }
