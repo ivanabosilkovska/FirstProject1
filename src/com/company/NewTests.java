364a86939a31b2d9a25a05786e19083cdf0ce342
Ivana <bosilkovska.ivana@gmail.com>
2021-08-30 21:09:12 +0200

    name class added

---
+++ src/com/company/NewTests.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class NewTests {
+
+    public NewTests() {
+    }
+
+    private  int a = 10;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
