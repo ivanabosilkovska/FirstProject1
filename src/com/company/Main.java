94e58ce6b285e1e4129d26ca71c82caf3a4caa62
trajceivanovski <trajceivanovski647@gmail.com>
2021-09-02 18:06:18 +0200

    My new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 class HelloWorld{
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello Ivana!!!!!");
 
         System.out.println("Hello");
         String name = "Ivana";
