76ddcb576d4600e71c8e032e0ee18dad7645bcba
Ivana <bosilkovska.ivana@gmail.com>
2021-11-22 21:27:02 +0100

    added new validation

--- src/com/company/NewTestingClass.java
+++ src/com/company/NewTestingClass.java
@@ -3,5 +3,7 @@ package com.company;
 public class NewTestingClass {
     public static void main(String[] args) {
         System.out.println("testing");
+
+        System.out.println("GIT");
     }
 }
