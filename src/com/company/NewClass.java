7ec49b197d8879e05c9a3db25228a3406c7e41e2
Ivana <bosilkovska.ivana@gmail.com>
2021-11-22 21:06:31 +0100

    testing

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -4,7 +4,7 @@ public class NewClass {
 
     public static void main(String[] args) {
         System.out.println("Learning GIT is fun..");
-        System.out.println("Hellooo!");
+        System.out.println("Hellooo again!");
 
         int a = 15;
 
