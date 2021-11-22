59043efdd2faff5bef8a24f68ff81ee660e59e4f
Ivana <bosilkovska.ivana@gmail.com>
2021-11-22 21:04:16 +0100

    learning git commit and push

--- src/com/company/NewClass.java
+++ src/com/company/NewClass.java
@@ -4,6 +4,7 @@ public class NewClass {
 
     public static void main(String[] args) {
         System.out.println("Learning GIT is fun..");
+        System.out.println("Hellooo!");
 
         int a = 15;
 
